c97bb98992d2bd35beb171302aa4e9494635553a
mohamad ganji <mohamad.ganjif@gmail.com>
2023-07-11 15:50:27 +0330

    bug of star is not solved

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
+import StarRating from "./StarRating";
+// import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <StarRating maxRating={10} />
   </React.StrictMode>
 );
